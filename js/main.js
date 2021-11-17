9c030662be089273989e0cadf010a9194f2a1180
Mona Lisa <mykolakratasyk@gmail.com>
2021-11-17 17:18:26 +0300

    start on ready document

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-function progress() {
-    let elem = document.getElementById('progressbar__line');
-    let label = document.getElementById('progressbar__label');
-    let startWidth = 0;
-    let id = setInterval(progressStatus, 100);
+$(document).ready(function() {
+    function progress() {
+        let elem = document.getElementById('progressbar__line');
+        let label = document.getElementById('progressbar__label');
+        let startWidth = 0;
+        let id = setInterval(progressStatus, 100);
 
-    function progressStatus() {
-        if (startWidth >= 100) {
-            clearInterval(id);
-        } else {
-            startWidth++;
-            elem.style.width = startWidth + '%';
-            label.innerHTML = startWidth * 1 + '%';
+        function progressStatus() {
+            if (startWidth >= 100) {
+                clearInterval(id); // остановить дальгейшее выполнение функции 
+            } else {
+                startWidth++;
+                elem.style.width = startWidth + '%';
+                label.innerHTML = startWidth * 1 + '%';
+            }
         }
+
     }
 
-}
-
-progress();
+    progress();
+});
